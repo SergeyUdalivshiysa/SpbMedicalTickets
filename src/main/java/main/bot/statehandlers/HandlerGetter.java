a025e37a9c60059280231702c52365b05b5a9155
Sergey <chigagog@gmail.com>
2021-04-26 14:56:13 +0300

    getHandlerName method is optimised

--- src/main/java/main/bot/statehandlers/HandlerGetter.java
+++ src/main/java/main/bot/statehandlers/HandlerGetter.java
@@ -5,6 +5,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Component;
 
+import java.util.Arrays;
+import java.util.Optional;
+
 @Component
 public class HandlerGetter {
 
@@ -18,7 +21,24 @@ public class HandlerGetter {
         return (Handler) context.getBean(getHandlerName(botState));
     }
 
+
+
+
     private String getHandlerName(BotState botState) {
+        String botStateName = botState.toString().toLowerCase();
+        String[] strings = botStateName.split("_");
+        Optional<String> output = Arrays.stream(strings)
+                .map(word -> word.equals(strings[0]) ? word : makeFirstLetterUpperCase(word))
+                .reduce((x, y) -> x + y);
+        return  output.get() + "Handler";
+    }
+
+    private static String makeFirstLetterUpperCase(String input) {
+        char ch = input.charAt(0);
+        return String.valueOf(ch).toUpperCase() + input.substring(1);
+    }
+
+/*    private String getHandlerName(BotState botState) {
         String botStateName = botState.toString().toLowerCase();
         StringBuilder stringBuilder = new StringBuilder(botStateName);
         for (int i = 0; i < stringBuilder.length(); i++) {
@@ -30,6 +50,6 @@ public class HandlerGetter {
         }
         stringBuilder.append("Handler");
         return stringBuilder.toString();
-    }
+    }*/
 
 }
